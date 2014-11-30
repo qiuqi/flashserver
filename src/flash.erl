b2ffc2fcf7d97f8b7f7d40382dbf737fea34d610
qiuqi <qiu.qiq@gmail.com>
2014-11-30 11:23:56 +0800

    initial commit

---
+++ src/flash.erl
@@ -0,0 +1,30 @@
+%% @author Mochi Media <dev@mochimedia.com>
+%% @copyright 2010 Mochi Media <dev@mochimedia.com>
+
+%% @doc flash.
+
+-module(flash).
+-author("Mochi Media <dev@mochimedia.com>").
+-export([start/0, stop/0]).
+
+ensure_started(App) ->
+    case application:start(App) of
+        ok ->
+            ok;
+        {error, {already_started, App}} ->
+            ok
+    end.
+
+
+%% @spec start() -> ok
+%% @doc Start the flash server.
+start() ->
+    flash_deps:ensure(),
+    ensure_started(crypto),
+    application:start(flash).
+
+
+%% @spec stop() -> ok
+%% @doc Stop the flash server.
+stop() ->
+    application:stop(flash).
---
+++ start-dev.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+exec erl +K true +P 10000 -ssl \
+    -pa ebin deps/*/ebin \
+    -boot start_sasl \
+    -config dev\
+    -sname flash_dev \
+    -s flash \
+    -s reloader
